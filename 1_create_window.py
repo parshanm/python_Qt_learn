457ac1809ae9523254e601f4fb0e3c56a82cdd24
Parshan <parshan.m.m@gmail.com>
2025-06-07 22:03:30 +0330

    first file added.

---
+++ 1_create_window.py
@@ -0,0 +1,18 @@
+# import:
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+#create a app
+app = QApplication([])
+
+#create a window
+window = QWidget()
+
+#set a title:
+window.setWindowTitle('My first app')
+
+#show this window
+window.show()
+
+#keep window on screen
+sys.exit(app.exec())
